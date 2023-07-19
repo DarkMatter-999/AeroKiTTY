aaa8ff1803ded9769a17d7f5af9ec5dd8c70a795
DarkMatter-999 <darkmatter999official@gmail.com>
2023-07-19 18:27:14 +0530

    Implemented text display

--- aerokitty_core/gui.go
+++ aerokitty_core/gui.go
@@ -1,8 +1,12 @@
 package aerokitty_core
 
 import (
-	"log"
+	"bufio"
+	"io"
 	"os"
+	"time"
+
+	//"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -17,28 +21,73 @@ func CreateTermWindow(filePath string) {
 	ui := widget.NewTextGrid()      
 	ui.SetText("Welcome to AeroKiTTY")
 
+	shell, err := spawnShell()
+	if err != nil {
+		os.Exit(0)
+	}
+
+	onKeyType := func(e *fyne.KeyEvent) {
+		if e.Name == fyne.KeyEnter || e.Name == fyne.KeyReturn {
+			_, _ = shell.Write([]byte{'\r'})
+		}
+	}
+
+	onTypeChar := func(r rune) {
+		_, _ = shell.Write([]byte{byte(r)})
+	}
+
+	window.Canvas().SetOnTypedKey(onKeyType)
+	window.Canvas().SetOnTypedRune(onTypeChar)
+
+	buffer := newTerm()
+	reader := bufio.NewReader(shell)
+
+	go func() {
+		line := []rune{}
+		buffer.buffer = append(buffer.buffer, line)
+
+		for {
+			r, _, err := reader.ReadRune()
+			if err != nil {
+				if err == io.EOF {
+					return
+				}
+				fyne.LogError("Error: %s", err)
+				os.Exit(-1)
+			}
+
+			line = append(line, r)
+			buffer.buffer[len(buffer.buffer) - 1] = line
+			if r == '\n' {
+				if len(buffer.buffer) > MaxBufferSize {
+					buffer.buffer = buffer.buffer[1:]
+				}
+
+				line = []rune{}
+				buffer.buffer = append(buffer.buffer, line)
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(100 * time.Millisecond)
+			var lines string
+			for _, line := range buffer.buffer {
+				lines = lines +  string(line)
+			}
+			ui.SetText("")
+			ui.SetText(string(lines))
+		}
+	}()
+
+
 	window.SetContent(
 		fyne.NewContainerWithLayout(
 			layout.NewGridWrapLayout(fyne.NewSize(800, 600)),
 			ui,
 		),
 	)
-
-	shell, err := spawnShell()
-	if err != nil {
-		os.Exit(0)
-	}
-
-
-	shell.Write([]byte("dir\r"))
-	//time.Sleep(1 * time.Second)
-	b := make([]byte, 1024)
-	_, err = shell.Read(b)
-	if err != nil {
-		log.Printf("Failed to read pty %s", err)
-	}
-	log.Println(b)
-
 
 	window.ShowAndRun()
 }
--- aerokitty_core/term.go
+++ aerokitty_core/term.go
@@ -3,4 +3,14 @@ package aerokitty_core
 import (
 )
 
+const MaxBufferSize = 16
 
+type Terminal struct {
+	buffer [][]rune
+}
+
+func newTerm() *Terminal {
+	return &Terminal{
+		buffer: [][]rune{},
+	}
+}
